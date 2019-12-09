532ee9a023f14d0aef4d3c9fc0f6031b89980087
wu_yang <wuyang@xiniaoyun.com>
2019-12-09 16:25:57 +0800

    修改了用户获取信息

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,7 +5,8 @@ import { resetRouter } from '@/router'
 const state = {
   token: getToken(),
   name: '',
-  avatar: ''
+  avatar: '',
+  username: ''
 }
 
 const mutations = {
@@ -15,6 +16,9 @@ const mutations = {
   SET_NAME: (state, name) => {
     state.name = name
   },
+  SET_USERNAME: (state, username) => {
+    state.username = username
+  },
   SET_AVATAR: (state, avatar) => {
     state.avatar = avatar
   }
@@ -23,14 +27,20 @@ const mutations = {
 const actions = {
   // user login
   login({ commit }, userInfo) {
+    // console.log('从登录页面获取的userinfo信息' + JSON.stringify(userInfo))
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
+      // 需要修改request.js里的内容
       login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
+        // console.log('登录成功返回信息:' + JSON.stringify(response))
+        const { access } = response
+        // 存储到store中
+        commit('SET_TOKEN', access)
+        // 存储到Cookies中
+        setToken(access)
         resolve()
       }).catch(error => {
+        console.log(error)
         reject(error)
       })
     })
@@ -40,17 +50,15 @@ const actions = {
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
       getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
+        // const { data } = response
+        // console.log(data)
+        if (!response) {
           reject('Verification failed, please Login again.')
         }
-
-        const { name, avatar } = data
-
+        const { name, username } = response
         commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
+        commit('SET_USERNAME', username)
+        resolve(response)
       }).catch(error => {
         reject(error)
       })
@@ -87,4 +95,3 @@ export default {
   mutations,
   actions
 }
-
